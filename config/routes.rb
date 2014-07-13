8c639b6dda17d08b204753b59395633fb90343f0
Can <can@martin.com.tw>
2014-07-13 18:05:17 +0800

    root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'products/index'
 
-  get 'products/show'
 
+  root :to => 'products#index'
+  resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
